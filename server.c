bcc7a00c6adb18055af331aed93925ab854bca13
Kajzerr <pawel_banbula@wp.pl>
2019-05-23 23:06:24 +0200

    demon serwer

--- server.c
+++ server.c
@@ -3,9 +3,18 @@
 #include <string.h>
 #include <unistd.h>
 #include <time.h>
-#include <sys/types.h> 
+#include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+
+#include <syslog.h>
+#include <netdb.h>
+#include <signal.h>
+#include <sys/time.h>
+#include <arpa/inet.h>
+#include <errno.h>
+#include <fcntl.h>
+
 
 #include "lib/msg.h"
 #include "lib/map.h"
@@ -14,8 +23,53 @@
 #include "lib/config.h"
 
 #define DEBUG
+#define	MAXFD	64
 
 int num_of_games = 0;
+
+int daemon_init(const char *pname, int facility, uid_t uid, int socket)
+{
+    int		i, p;
+    pid_t	pid;
+
+    if ( (pid = fork()) < 0)
+        return (-1);
+    else if (pid)
+        exit(0);			/* parent terminates */
+
+    /* child 1 continues... */
+
+    if (setsid() < 0)			/* become session leader */
+        return (-1);
+
+    signal(SIGHUP, SIG_IGN);
+    if ( (pid = fork()) < 0)
+        return (-1);
+    else if (pid)
+        exit(0);			/* child 1 terminates */
+
+    /* child 2 continues... */
+
+    chroot("/tmp");
+
+    /* close off file descriptors */
+    for (i = 0; i < MAXFD; i++){
+        if(socket != i )
+            close(i);
+    }
+
+    /* redirect stdin, stdout, and stderr to /dev/null */
+    p= open("/dev/null", O_RDONLY);
+    open("/dev/null", O_RDWR);
+    open("/dev/null", O_RDWR);
+
+    openlog(pname, LOG_PID, facility);
+
+    syslog(LOG_ERR," STDIN =   %i\n", p);
+    setuid(uid); /* change user */
+
+    return (0);				/* success */
+}
 
 void error(const char *msg)
 {
@@ -31,24 +85,24 @@ int setup_listener(int portno)
 
     /* Get a socket to listen on */
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (sockfd < 0) 
+    if (sockfd < 0)
         error("ERROR opening listener socket.");
-    
+
     /* Zero out the memory for the server information */
     memset(&serv_addr, 0, sizeof(serv_addr));
-    
-	/* set up the server info */
-    serv_addr.sin_family = AF_INET;	
-    serv_addr.sin_addr.s_addr = INADDR_ANY;	
-    serv_addr.sin_port = htons(portno);		
+
+    /* set up the server info */
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = INADDR_ANY;
+    serv_addr.sin_port = htons(portno);
 
     /* Bind the server info to the listener socket. */
     if (bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0)
         error("ERROR binding listener socket.");
 
-    #ifdef DEBUG
-    printf("[DEBUG] Listener set.\n");    
-    #endif 
+#ifdef DEBUG
+    printf("[DEBUG] Listener set.\n");
+#endif
 
     /* Return the socket number. */
     return sockfd;
@@ -61,52 +115,52 @@ void get_clients(int lis_sockfd, int * cli_sockfd)
     struct sockaddr_in serv_addr, cli_addr;
     id_message id_msg;
     hold_message hold_msg;
-    
-    #ifdef DEBUG
+
+#ifdef DEBUG
     printf("[DEBUG] Listening for clients...\n");
-    #endif 
+#endif
 
     /* Listen for two clients. */
     int num_conn = 0;
     while(num_conn < 2)
     {
         /* Listen for clients. */
-	    listen(lis_sockfd, /*253 - player_count*/ 2);
-        
+        listen(lis_sockfd, /*253 - player_count*/ 2);
+
         /* Zero out memory for the client information. */
         memset(&cli_addr, 0, sizeof(cli_addr));
 
         clilen = sizeof(cli_addr);
-	
-	    /* Accept the connection from the client. */
+
+        /* Accept the connection from the client. */
         cli_sockfd[num_conn] = accept(lis_sockfd, (struct sockaddr *) &cli_addr, &clilen);
-    
+
         if (cli_sockfd[num_conn] < 0)
             /* Horrible things have happened. */
             error("ERROR accepting a connection from a client.");
 
-        #ifdef DEBUG
+#ifdef DEBUG
         printf("[DEBUG] Accepted connection from client %d\n", num_conn);
-        #endif 
-        
+#endif
+
         /* Send the client it's ID. */
         if(send_id_message(cli_sockfd[num_conn], num_conn) < 0)
             error("ERROR sending ID to client");
 
-        #ifdef DEBUG
-        printf("[DEBUG] Sent client %d it's ID.\n", num_conn); 
-        #endif 
-        
+#ifdef DEBUG
+        printf("[DEBUG] Sent client %d it's ID.\n", num_conn);
+#endif
+
         /* Increment the player count. */
 
         if (num_conn == 0) {
             /* Send "HLD" to first client to let the user know the server is waiting on a second client. */
             if(send_hold_message(cli_sockfd[0]) < 0)
                 error("ERROR sending HOLD to client");
-            
-            #ifdef DEBUG
+
+#ifdef DEBUG
             printf("[DEBUG] Told client 0 to hold.\n");
-            #endif 
+#endif
         }
 
         num_conn++;
@@ -134,85 +188,85 @@ int get_clients_coords(int *cli_sockfd, Map* map_p0, Map* map_p1){
 
     do
     {
-        #ifdef DEBUG
+#ifdef DEBUG
         printf("[DEBUG] Waiting for INSERT messages from clients...\n");
-        #endif
+#endif
         FD_SET(cli_sockfd[0], &rset);
         FD_SET(cli_sockfd[1], &rset);
         if ( ( n = select(maxfd+1, &rset, NULL, NULL, NULL) ) < 0){
             return -4;
         }
-        #ifdef DEBUG
+#ifdef DEBUG
         printf("[DEBUG] %d sockets ready for read.\n", n);
-        #endif
+#endif
         /* TO MOZE BYC BLAD GNIAZDA NIE KONIECZNIE READY READ*/
         if (FD_ISSET(cli_sockfd[0], &rset)) {
-            #ifdef DEBUG
+#ifdef DEBUG
             printf("[DEBUG] socket 0 is ready");
-            #endif
+#endif
             if( (msg_type = receive_message(cli_sockfd[0], &message) ) < 0 )
                 error("ERROR receiving message form client");
         }
         if (FD_ISSET(cli_sockfd[1], &rset)) {
-            #ifdef DEBUG
+#ifdef DEBUG
             printf("[DEBUG] socket 1 is ready");
-            #endif
+#endif
             if( (msg_type = receive_message(cli_sockfd[1], &message) ) < 0 )
                 error("ERROR receiving message form client");
         }
         if(msg_type == INSERT_MSG_TYPE){
-            #ifdef DEBUG
+#ifdef DEBUG
             printf("[DEBUG] recived INSERT message.\n");
-            #endif
+#endif
             if (insert_ship(
-                ((insert_message*)message)->id ? map_p1 : map_p0,
-                ((insert_message*)message)->ship,
-                ((insert_message*)message)->x,
-                ((insert_message*)message)->y,
-                ((insert_message*)message)->orientation
-                )== -1)
+                    ((insert_message*)message)->id ? map_p1 : map_p0,
+                    ((insert_message*)message)->ship,
+                    ((insert_message*)message)->x,
+                    ((insert_message*)message)->y,
+                    ((insert_message*)message)->orientation
+            )== -1)
                 error("ERROR inserting ship based on recived client data");
 
             ships_left_p0 = check_used_ships(map_p0);
             ships_left_p1 = check_used_ships(map_p1);
-            #ifdef DEBUG
+#ifdef DEBUG
             printf("[DEBUG] Ships left to assign: Player0=%d, Player1=%d\n", ships_left_p0, ships_left_p1);
-            #endif
+#endif
 
             if(ships_left_p0 == 0){
-                #ifdef DEBUG
+#ifdef DEBUG
                 printf("[DEBUG] Recived all player0's coordinates\n");
-                #endif
+#endif
                 if(!hold_sent){
                     if(send_hold_message(cli_sockfd[0]) < 0)
                         error("ERROR sending HOLD to client");
                     hold_sent = 1;
-                    #ifdef DEBUG
+#ifdef DEBUG
                     printf("[DEBUG] sent HOLD to player0\n");
-                    #endif
+#endif
                 }
             }
             if(ships_left_p1 == 0){
-                #ifdef DEBUG
+#ifdef DEBUG
                 printf("[DEBUG] Recived all player1's coordinates\n");
-                #endif
+#endif
                 if(!hold_sent){
                     if(send_hold_message(cli_sockfd[1]) < 0)
                         error("ERROR sending HOLD to client");
                     hold_sent = 1;
-                    #ifdef DEBUG
+#ifdef DEBUG
                     printf("[DEBUG] sent HOLD to player1\n");
-                    #endif
+#endif
                 }
             }
-            
+
         }else
             error("ERROR wrong message type, expected INSERT message");
     }while(ships_left_p0 > 0 || ships_left_p1 > 0);
 
-    #ifdef DEBUG
+#ifdef DEBUG
     printf("[DEBUG] Both players' ship coordinates recived. Starting the game...\n");
-    #endif
+#endif
     return 1;
 }
 
@@ -225,16 +279,16 @@ void run_game(int *cli_sockfd /*void *thread_data*/)
     /* Create maps for player 1 and 2*/
     Map *map_p0 = init_map_matrix(MAP_WIDTH, MAP_HEIGH);
     Map *map_p1 = init_map_matrix(MAP_WIDTH, MAP_HEIGH);
-    
+
     /* Send the start message. */
     if(send_start_message(cli_sockfd[0]) < 0)
         error("ERROR sending START to client1");
     if(send_start_message(cli_sockfd[1]) < 0)
         error("ERROR sending START to client2");
 
-    #ifdef DEBUG
+#ifdef DEBUG
     printf("[DEBUG] Sent start message.\n");
-    #endif
+#endif
 
     /* Initialize map for both players*/
     get_clients_coords( cli_sockfd, map_p0, map_p1);
@@ -250,79 +304,79 @@ void run_game(int *cli_sockfd /*void *thread_data*/)
         error("ERROR sending BEGIN to client2");
 
     show_maps(map_p0, map_p1);
-    
+
     int game_over = 0;
     int player_turn = starting_id;
     int msg_type, code;
     void* message = NULL;
     while(!game_over) {
-        #ifdef DEBUG
+#ifdef DEBUG
         printf("[DEBUG] Waiting for Player%d's move\n", player_turn);
-        #endif
+#endif
         if( (msg_type = receive_message(cli_sockfd[player_turn], &message) ) < 0 )
-                error("ERROR receiving message form client");
+            error("ERROR receiving message form client");
         if(msg_type == ATTACK_MSG_TYPE){
-            #ifdef DEBUG
+#ifdef DEBUG
             printf("[DEBUG] recived ATTACK message\n");
-            #endif
+#endif
             Map *map = ((attack_message*)message)->id ? map_p0 : map_p1;
             if ( ( code = attack_ship(map, ((attack_message*)message)->x, ((attack_message*)message)->y) ) ){
 
                 if (check_map(map) == 0)
                 {
-                    #ifdef DEBUG
+#ifdef DEBUG
                     printf("[DEBUG] GAME OVER\n");
-                    #endif
+#endif
                     if(send_status_message(cli_sockfd[0], ((attack_message*)message)->id, ((attack_message*)message)->x, ((attack_message*)message)->y, GAMEOVER, 0) < 0)
                         error("ERROR sending STATUS to client");
                     if(send_status_message(cli_sockfd[1], ((attack_message*)message)->id, ((attack_message*)message)->x, ((attack_message*)message)->y, GAMEOVER, 0) < 0)
                         error("ERROR sending STATUS to client");
-                    #ifdef DEBUG
+#ifdef DEBUG
                     printf("[DEBUG] sent STATUS message\n");
-                    #endif
+#endif
                     break;
                 }
                 else
                 {
                     if(code == 1){
-                        #ifdef DEBUG
+#ifdef DEBUG
                         printf("[DEBUG] HIT\n");
-                        #endif
+#endif
                         if(send_status_message(cli_sockfd[0], ((attack_message*)message)->id, ((attack_message*)message)->x, ((attack_message*)message)->y, HIT, 0) < 0)
                             error("ERROR sending STATUS to client");
                         if(send_status_message(cli_sockfd[1], ((attack_message*)message)->id, ((attack_message*)message)->x, ((attack_message*)message)->y, HIT, 0) < 0)
                             error("ERROR sending STATUS to client");
-                        #ifdef DEBUG
+#ifdef DEBUG
                         printf("[DEBUG] sent STATUS message\n");
-                        #endif
+#endif
                     }else if(code == 2){
-                        #ifdef DEBUG
+#ifdef DEBUG
                         printf("[DEBUG] SUNK\n");
-                        #endif
+#endif
                         int ship_type = getType(map, ((attack_message*)message)->x, ((attack_message*)message)->y );
                         if(send_status_message(cli_sockfd[0], ((attack_message*)message)->id, ((attack_message*)message)->x, ((attack_message*)message)->y, SUNK, ship_type) < 0)
                             error("ERROR sending STATUS to client");
                         if(send_status_message(cli_sockfd[1], ((attack_message*)message)->id, ((attack_message*)message)->x, ((attack_message*)message)->y, SUNK, ship_type) < 0)
                             error("ERROR sending STATUS to client");
-                        #ifdef DEBUG
+#ifdef DEBUG
                         printf("[DEBUG] sent STATUS message\n");
-                        #endif
+#endif
                     }
                 }
             }else{
-                #ifdef DEBUG
+#ifdef DEBUG
                 printf("[DEBUG] MISS\n");
-                #endif
+#endif
                 if(send_status_message(cli_sockfd[0], ((attack_message*)message)->id, ((attack_message*)message)->x, ((attack_message*)message)->y, MISS, 0) < 0)
                     error("ERROR sending STATUS to client");
                 if(send_status_message(cli_sockfd[1], ((attack_message*)message)->id, ((attack_message*)message)->x, ((attack_message*)message)->y, MISS, 0) < 0)
                     error("ERROR sending STATUS to client");
-                #ifdef DEBUG
+#ifdef DEBUG
                 printf("[DEBUG] sent STATUS message\n");
-                #endif
+#endif
                 player_turn = !player_turn; //change player turn
             }
-            show_maps(map_p0, map_p1);  
+            show_maps(map_p0, map_p1);
         }
         else
             error("ERROR wrong message type, expected ATTACK message");
@@ -330,39 +384,42 @@ void run_game(int *cli_sockfd /*void *thread_data*/)
 
     printf("Game over.\n");
 
-	/* Close client sockets and decrement player counter. */
+    /* Close client sockets and decrement player counter. */
     close(cli_sockfd[0]);
     close(cli_sockfd[1]);
-    
+
     free(cli_sockfd);
 }
 
-/* 
+/*
  * Main Program
  */
 
 int main(int argc, char *argv[])
-{   
+{
     /* Make sure a port was specified. */
     if (argc < 2) {
         fprintf(stderr,"ERROR, no port provided\n");
         exit(1);
     }
-    
-    int lis_sockfd = setup_listener(atoi(argv[1])); /* Listener socket. */
 
-    while (1) { 
+    int lis_sockfd = setup_listener(atoi(argv[1])); /* Listener socket. */
+    int daemon_check = daemon_init(argv[0],LOG_USER,1111,lis_sockfd);
+    syslog (LOG_NOTICE, "Program started by User %d", getuid ());
+    syslog (LOG_INFO,"Waiting for clients ... ");
+
+    while (1) {
         if( (num_of_games++) < 10) { /* maximum number of hosted games */
             int *cli_sockfd = (int*)malloc(2*sizeof(int)); /* Client sockets */
             memset(cli_sockfd, 0, 2*sizeof(int));
-            
+
             /* Get two clients connected. */
             get_clients(lis_sockfd, cli_sockfd);
-            
-            #ifdef DEBUG
+
+#ifdef DEBUG
             printf("[DEBUG] Starting new game thread...\n");
-            #endif
-            
+#endif
+
             if ( fork() == 0) {    /* child process */
                 close(lis_sockfd);    /* close listening socket */
                 run_game(cli_sockfd);   /* process the request */
@@ -370,10 +427,10 @@ int main(int argc, char *argv[])
             }
             close(cli_sockfd[0]);
             close(cli_sockfd[1]);
-            
-            #ifdef DEBUG
+
+#ifdef DEBUG
             printf("[DEBUG] New game thread started.\n");
-            #endif
+#endif
         }
     }
 
